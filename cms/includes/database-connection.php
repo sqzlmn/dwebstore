5cad749eee18c6b8f0c8a757e8bce3939746d1e2
sqzlmn <adrianelouiscadawan@gmail.com>
2026-03-12 15:57:41 +0800

    Add files via upload

---
+++ cms/includes/database-connection.php
@@ -0,0 +1,26 @@
+<?php
+
+$type = 'mysql';
+$server = 'localhost';
+$db = 'pkmndatabse2';
+$port = '3306';
+$charset = 'utf8mb4';
+
+$username = 'root';
+$password = '';
+
+$options = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES => false,
+];
+
+$dsn ="$type:host=$server;dbname=$db;port=$port;charset=$charset";
+
+try {
+    $pdo = new PDO($dsn, $username, $password, $options);
+} catch (PDOException $e) {
+    throw new PDOException($e->getMessage(), $e->getCode());
+}
+
+?>
---
+++ cms/includes/footer.php
@@ -0,0 +1,8 @@
+    <footer>
+      <div class="container">
+        &copy; Adriane Cadawan | CYB201 <?= date('Y'); ?>
+      </div>
+    </footer>
+    <script src="js/site.js"></script>
+  </body>
+</html>
